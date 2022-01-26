ad7f28dc5d6262268d51283931d0209e7419c0ff
elifcetinkaya <elfctnkaya1@gmail.com>
2022-01-26 15:32:43 +0300

    movies app with kotlin

--- app/src/main/java/com/example/choosemovies/ui/MoviesDetailFragment.kt
+++ app/src/main/java/com/example/choosemovies/ui/MoviesDetailFragment.kt
@@ -1,14 +1,13 @@
 package com.example.choosemovies.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import coil.load
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.example.choosemovies.R
